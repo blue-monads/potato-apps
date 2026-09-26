6ce83b6852f85f40fba8ccf7508207e5bed17c0e
bornjre <bornjre@gmail.com>
2026-09-26 23:18:05 +0545

    placeholder stuff

---
+++ cimple-table/ui/src/pages/autodash/AutoDash.tsx
@@ -0,0 +1,7 @@
+import { useState, useEffect, useMemo } from "react";
+import { useParams, useNavigate, Link } from "react-router";
+
+
+export default function AutoDash() {
+    return <div>AutoDash</div>;
+}
